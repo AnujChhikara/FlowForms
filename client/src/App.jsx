2d654ccae86658fce16e02b6501ae47d66970560
AnujChhikara <anujchhikara777@gmail.com>
2024-11-04 17:15:05 +0530

    changed the ui for login and register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
-import { LoginPage } from "./Pages/Login";
+import LoginPage from "./Pages/Login";
 import RegisterPage from "./Pages/Register";
 
 const AllRoutes = () => {
